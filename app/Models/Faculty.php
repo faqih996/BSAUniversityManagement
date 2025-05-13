022e400b19d732978debf6bc60b8ce0a173cf1d6
faqih <faqih.syakir11@gmail.com>
2025-05-13 13:58:00 +0700

    fetch:factory & seeder

--- app/Models/Faculty.php
+++ app/Models/Faculty.php
@@ -3,11 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Faculty extends Model
 {
+
+    use HasFactory;
+
     protected $fillable = [
         'name',
         'code',
